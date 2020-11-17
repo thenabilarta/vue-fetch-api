b77ba259badd7776340c9ce71e9d32440bf53f8d
thenabilarta <thenabilarta@gmail.com>
2020-11-17 11:13:48 +0700

    Initial commit

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,69 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app" class="container">
+    <div class="row">
+      <div class="col-8">
+        <ul class="nav py-3">
+          <li class="nav-item">
+            <a class="nav-link active" href="#">Active</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Link</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Link</a>
+          </li>
+          <li class="nav-item">
+            <a
+              class="nav-link disabled"
+              href="#"
+              tabindex="-1"
+              aria-disabled="true"
+              >Disabled</a
+            >
+          </li>
+        </ul>
+      </div>
+    </div>
+
+    <div class="row">
+      <div class="col-8">
+        <p>{{ user.length }}</p>
+        <p v-for="u in user" :key="u.id">
+          {{ u }}
+        </p>
+      </div>
+    </div>
+
+    <HelloWorld msg="Javascript" />
+    <Menu v-on:termChange="onTermChange"></Menu>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import axios from "axios";
+import HelloWorld from "./components/HelloWorld.vue";
+import Menu from "./components/Menu.vue";
 
 export default {
-  name: 'App',
+  data: function() {
+    return {
+      user: [],
+    };
+  },
+  name: "App",
   components: {
-    HelloWorld
-  }
-}
+    HelloWorld,
+    Menu,
+  },
+  methods: {
+    onTermChange: function() {
+      axios.get("https://jsonplaceholder.typicode.com/users").then((res) => {
+        console.log(res.data);
+        res.data.map((r) => this.user.push(r.name));
+      });
+    },
+  },
+};
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+<style></style>
---
+++ src/components/Menu.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <button class="btn btn-primary" @click="click">Fetch</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Menu",
+  methods: {
+    click: function() {
+      this.$emit("termChange");
+    },
+  },
+};
+</script>
+
+<style scoped></style>
